e425cff92b135bd45c4eb6469e22991a3db0767f
Alex Lyzhoft <alexlyz@gmail.com>
2017-10-19 22:51:17 -0500

    Added toolbar to home page using Vuetify

---
+++ client/src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-app>
+      <v-layout column>
+        <v-flex>
+          <v-toolbar fixed class="purple darken-1" dark>
+            <v-toolbar-title >A&S</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
+            <v-toolbar-side class="hidden-sm-and-down">
+              <v-btn flat>About Us</v-btn>
+              <v-btn flat>R.S.V.P</v-btn>
+              <v-btn flat>Venue/Hotel Information</v-btn>
+              <v-btn flat>Registry</v-btn>
+              <v-btn flat>Music Request</v-btn>
+            </v-toolbar-side>
+          </v-toolbar>
+        </v-flex>
+      </v-layout>
+    </v-app>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="hello">
-    <h1>Register</h1>
-
-    <input type="email" name="email" placeholder="email" v-model="email">
-    <br>
-    <input type="password" name="password" placeholder="password" v-model="password">
-    <br>
-    <button @click="register">Register</button>
+  <div>
+    <v-app>
+      <v-layout column>
+        <v-flex xs6 offset-xs3>
+          <div class="white elevation-2">
+            <v-toolbar flat dense class="cyan" dark>
+              <v-toolbar-title>Register</v-toolbar-title>
+            </v-toolbar>
+            <div class="pl-4 pr-4 pt-2 pb-2">
+              <v-text-field type="email" name="email" placeholder="email" v-model="email"></v-text-field>
+              <br>
+              <v-text-field type="password" name="password" placeholder="password" v-model="password"></v-text-field>
+              <br>
+              <v-btn @click="register" class="cyan" dark>Register</v-btn>
+            </div>
+          </div>
+        </v-flex>
+      </v-layout>
+    </v-app>
   </div>
 </template>
 
